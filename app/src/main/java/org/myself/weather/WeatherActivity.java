4f620212dc33fb979ea319128870bc045272158a
Razaul Munir <razaulmunir@gmail.com>
2020-05-05 20:05:51 +0600

    :

--- app/src/main/java/org/myself/weather/WeatherActivity.java
+++ app/src/main/java/org/myself/weather/WeatherActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    String APP_ID="bd8a9ac5b7a888e5043810688582f61f";
+    final String WEATHER_URL="http://api.openweathermap.org/data/2.5/weather";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
