1be7a242f8386e776ea39e0fd7186995ebec1cd1
Razaul Munir <razaulmunir@gmail.com>
2020-05-26 12:59:54 +0600

    round button added

---
+++ app/src/main/java/org/myself/weather/WeatherOrActivity.java
@@ -0,0 +1,14 @@
+package org.myself.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherOrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_or);
+    }
+}
