ea961095baf18884dfd95c5766dccfdc0c2b484c
Razaul Munir <razaulmunir@gmail.com>
2020-05-26 13:09:36 +0600

    weather layout added

--- app/src/main/java/org/myself/weather/WeatherOrActivity.java
+++ app/src/main/java/org/myself/weather/WeatherOrActivity.java
@@ -3,6 +3,7 @@ package org.myself.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class WeatherOrActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class WeatherOrActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_or);
+    }
+
+    public void goForCity(View view) {
     }
 }
